81bc2fd8a74b9ab17fb065dc915af332973be9e8
kulas115 <kulas115@gmail.com>
2024-01-26 22:33:13 +0100

    Add test

--- cookbooks/database/test/integration/default/default_test.rb
+++ cookbooks/database/test/integration/default/default_test.rb
@@ -1,3 +1,7 @@
 describe bash('psql --version') do
   its('stdout') { is_expected.to include '12.17' }
 end
+
+describe bash('PGPASSWORD=12345 psql -h localhost -U postgres -c "\l"') do
+  its('stdout') { is_expected.to include 'rails_db' }
+end
